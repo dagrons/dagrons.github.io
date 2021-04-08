473fb987804f884a05843b171a38f57f4cb69947
dagrons <heyuehuii@126.com>
2021-04-08 22:59:02 +0800

    Nikola auto commit.
    Nikola version: 8.1.3

--- cache/.mako.tmp/tags.tmpl.py
+++ cache/.mako.tmp/tags.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1617677997.2903762
+_modified_time = 1617893942.1459923
 _enable_loop = True
 _template_filename = '/home/heyuehui/envs/blog/lib/python3.8/site-packages/nikola/data/themes/bootstrap4/templates/tags.tmpl'
 _template_uri = 'tags.tmpl'
@@ -29,15 +29,15 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         cat_items = context.get('cat_items', UNDEFINED)
-        cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
-        len = context.get('len', UNDEFINED)
-        def content():
-            return render_content(context._locals(__M_locals))
-        items = context.get('items', UNDEFINED)
-        range = context.get('range', UNDEFINED)
-        title = context.get('title', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
+        items = context.get('items', UNDEFINED)
+        len = context.get('len', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
+        range = context.get('range', UNDEFINED)
+        def content():
+            return render_content(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
@@ -54,15 +54,15 @@ def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         cat_items = context.get('cat_items', UNDEFINED)
-        cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
-        len = context.get('len', UNDEFINED)
-        def content():
-            return render_content(context)
-        items = context.get('items', UNDEFINED)
-        range = context.get('range', UNDEFINED)
-        title = context.get('title', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
+        items = context.get('items', UNDEFINED)
+        len = context.get('len', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
+        range = context.get('range', UNDEFINED)
+        def content():
+            return render_content(context)
         __M_writer = context.writer()
         __M_writer('\n<h1>')
         __M_writer(filters.html_escape(str(title)))
---
+++ posts/torch.md
@@ -0,0 +1,12 @@
+<!--
+.. title: torch
+.. slug: torch
+.. date: 2021-04-08 22:58:49 UTC+08:00
+.. tags: 
+.. category: 
+.. link: 
+.. description: 
+.. type: text
+-->
+
+在这里书写你的文章。
