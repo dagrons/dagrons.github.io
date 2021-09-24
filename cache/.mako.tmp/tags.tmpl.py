dfea451f3145d1b3b25db7bf16ce0b4c8cf9397e
heyuehui <heyuehuii@126.com>
2021-09-24 10:57:41 +0800

    Nikola auto commit.
    Nikola version: 8.1.3

--- cache/.mako.tmp/tags.tmpl.py
+++ cache/.mako.tmp/tags.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1632409233.099142
+_modified_time = 1632452260.676453
 _enable_loop = True
 _template_filename = '/home/ubuntu/Envs/nikola/lib/python3.6/site-packages/nikola/data/themes/bootstrap4/templates/tags.tmpl'
 _template_uri = 'tags.tmpl'
@@ -28,14 +28,14 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        messages = context.get('messages', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        range = context.get('range', UNDEFINED)
         cat_items = context.get('cat_items', UNDEFINED)
-        items = context.get('items', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        messages = context.get('messages', UNDEFINED)
         cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
-        range = context.get('range', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        items = context.get('items', UNDEFINED)
         len = context.get('len', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
         __M_writer = context.writer()
@@ -53,14 +53,14 @@ def render_body(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        messages = context.get('messages', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        range = context.get('range', UNDEFINED)
         cat_items = context.get('cat_items', UNDEFINED)
-        items = context.get('items', UNDEFINED)
         def content():
             return render_content(context)
+        messages = context.get('messages', UNDEFINED)
         cat_hierarchy = context.get('cat_hierarchy', UNDEFINED)
-        range = context.get('range', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        items = context.get('items', UNDEFINED)
         len = context.get('len', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
         __M_writer = context.writer()
